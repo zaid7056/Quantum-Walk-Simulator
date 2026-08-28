be420cbab63b396a2fc9a2bc1fbfc634e2209ec2
zaid-iq54 <kmishima322@gmail.com>
2026-08-29 00:28:03 +0530

    Update quantum_walk.py

--- quantum_walk.py
+++ quantum_walk.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 num_steps = 50 
 num_positions = 2 * num_steps + 1 
-
+#iniial state of the photon, currently set to purely horizontal polarization (1H, 0V):
 H_amplitudes = np.zeros(num_positions, dtype=complex)
 V_amplitudes = np.zeros(num_positions, dtype=complex)
 
@@ -11,7 +11,7 @@ center = num_steps
 
 H_amplitudes[center] = 1.0  
 V_amplitudes[center] = 0.0
-
+#coin angle:
 theta = np.pi / 8  
 
 for step in range(num_steps):
@@ -20,11 +20,11 @@ for step in range(num_steps):
     
     H_amplitudes = np.roll(new_H, 1)
     V_amplitudes = np.roll(new_V, -1)
-
+#probabilty at each position:
 probabilities = np.abs(H_amplitudes)**2 + np.abs(V_amplitudes)**2
 
 positions = np.arange(-num_steps, num_steps + 1)
-
+#plotting the graph:
 plt.figure(figsize=(10, 6))
 plt.bar(positions, probabilities, color='indigo', width=1.0)
 plt.title(f"1D Coined Quantum Walk after {num_steps} Steps")
